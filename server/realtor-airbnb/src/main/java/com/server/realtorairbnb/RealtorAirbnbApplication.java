ab115390e3e27be4a3a9a2731e66990bafba5ff6
Anthony Salemo <ajssalemo@gmail.com>
2021-02-06 11:10:30 -0500

    added a Spring Initialzr quickstart

---
+++ server/realtor-airbnb/src/main/java/com/server/realtorairbnb/RealtorAirbnbApplication.java
@@ -0,0 +1,13 @@
+package com.server.realtorairbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtorAirbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtorAirbnbApplication.class, args);
+	}
+
+}
---
+++ server/realtor-airbnb/src/test/java/com/server/realtorairbnb/RealtorAirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.realtorairbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtorAirbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
