d3ee9a1760613a0fefea528b814e07fbc09d7e15
Anthony Salemo <ajssalemo@gmail.com>
2021-02-07 20:39:06 -0500

    added a GET method to find users by id

--- server/realtor-airbnb/src/main/java/com/server/realtorairbnb/Controllers/UsersController.java
+++ server/realtor-airbnb/src/main/java/com/server/realtorairbnb/Controllers/UsersController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,15 @@ class UsersController {
 
     @PostMapping(path = "/add_user")
     public ResponseEntity<String> addUser(@RequestBody Users newUser) {
+        // Save the user
         usersRepository.save(newUser);
+        // Return a HTTP 201 'created' status
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(path = "/find_user/{id}")
+    public Users findUser(@PathVariable int id) {
+        // Find the user by ID, if there is no user, thrown an exception
+        return usersRepository.findById(id).orElseThrow(IllegalArgumentException::new);
     }
 }
