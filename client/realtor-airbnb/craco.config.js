4f2d51e48dae502a02c7673e0875bf98d25e21d6
Anthony Salemo <ajssalemo@gmail.com>
2021-02-10 20:59:50 -0500

    added path aliasing for better project structuring

--- client/realtor-airbnb/craco.config.js
+++ client/realtor-airbnb/craco.config.js
@@ -1,7 +1,16 @@
+const path = require("path");
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+  webpack: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
     },
   },
 };
--- client/realtor-airbnb/src/pages/home/Home.js
+++ client/realtor-airbnb/src/pages/home/Home.js
@@ -1,6 +1,6 @@
+import HomeBackgroundImageOne from "@images/backgrounds/home1.jpg";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import HomeBackgroundImageOne from "../../assets/images/backgrounds/home1.jpg";
 
 function App() {
   return (
