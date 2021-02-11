8f7563b24dbf3a05962473e5b5153a97288ec687
Anthony Salemo <ajssalemo@gmail.com>
2021-02-11 15:30:43 -0500

    added a informational box for the landing page

--- client/realtor-airbnb/src/components/landingnavigation/landingnavigation.js
+++ client/realtor-airbnb/src/components/landingnavigation/landingnavigation.js
@@ -1,7 +1,9 @@
 export default function LandingNavigation() {
   return (
-    <div className="h-1/4 w-1/2 bg-black absolute top-1/2 left-1/4 right-1/4 rounded-lg bg-opacity-75 border-solid border-4 border-white">
-      <h1 className="text-center text-white text-6xl">Realtor | AirBNB</h1>
+    <div className="h-1/4 w-3/4 sm:w-1/2 bg-black absolute top-1/2 left-1/10 sm:left-1/4 right-1/10 sm:right-1/4 rounded-lg bg-opacity-75 border-solid border-8 border-white ring-4 ring-opacity-25 ring-white ring-inset">
+      <h1 className="text-center text-white text-4xl md:text-5xl">
+        Realtor | AirBNB
+      </h1>
     </div>
   );
 }
--- client/realtor-airbnb/tailwind.config.js
+++ client/realtor-airbnb/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      inset: {
+        "1/10": "10%",
+      },
+    },
   },
   variants: {
     extend: {},
