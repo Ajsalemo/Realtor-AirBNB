6eee32d32fc6698d01104f65eb340f3a5568223e
Anthony Salemo <ajssalemo@gmail.com>
2021-02-11 15:10:20 -0500

    building a basic landing page layout to use as a base

---
+++ client/realtor-airbnb/src/components/landingnavigation/landingnavigation.js
@@ -0,0 +1,7 @@
+export default function LandingNavigation() {
+  return (
+    <div className="h-1/4 w-1/2 bg-black absolute top-1/2 left-1/4 right-1/4 rounded-lg bg-opacity-75 border-solid border-4 border-white">
+      <h1 className="text-center text-white text-6xl">Realtor | AirBNB</h1>
+    </div>
+  );
+}
--- client/realtor-airbnb/src/components/lazyloadimages/lazyloadimages.js
+++ client/realtor-airbnb/src/components/lazyloadimages/lazyloadimages.js
@@ -1,14 +1,16 @@
+import LandingNavigation from "@components/landingnavigation/landingnavigation";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function LazyLoadImages({ src, classNames }) {
   return (
-    <div className="h-screen">
+    <div className="h-screen relative">
       <LazyLoadImage
         src={src}
         alt="Images of homes for sale"
         effect="blur"
         className={classNames || null}
       />
+      <LandingNavigation />
     </div>
   );
 }
