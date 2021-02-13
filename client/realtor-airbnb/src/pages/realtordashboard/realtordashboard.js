de920a1578b73ee096421ea8f76dcbee6e0fcd30
Anthony Salemo <ajssalemo@gmail.com>
2021-02-12 22:55:26 -0500

    added a jumbotron type image for the realtor dashboard

--- client/realtor-airbnb/src/pages/realtordashboard/realtordashboard.js
+++ client/realtor-airbnb/src/pages/realtordashboard/realtordashboard.js
@@ -1,10 +1,16 @@
 import Footer from "@components/footer/footer";
 import Navbar from "@components/navbar/navbar";
+import RealtorDashboardBackgroundImage from "@images/backgrounds/realtor_dashboard_background.jpg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function RealtorDashboard() {
   return (
     <div>
       <Navbar />
+      <LazyLoadImage
+        src={RealtorDashboardBackgroundImage}
+        className="object-center object-cover w-full sm:h-96"
+      />
       Realtor Dashboard
       <Footer classNames="bg-gray-400 h-12 absolute bottom-0 w-full border-t-4 font-suez-one text-white text-center pt-1" />
     </div>
