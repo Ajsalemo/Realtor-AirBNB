6fee9136312d2631dfd3f96a1332c74c848b34b4
Anthony Salemo <ajssalemo@gmail.com>
2021-02-14 20:28:13 -0500

    corrected the ternary operator used to display messages based on the query results

--- client/realtor-airbnb/src/components/displaysuggestions/displaysuggestions.js
+++ client/realtor-airbnb/src/components/displaysuggestions/displaysuggestions.js
@@ -2,10 +2,12 @@ export default function DisplaySuggestions({ data }) {
   console.log(data);
   return (
     <div>
-      <h2>
+      <h2 className="font-suez-one text-center text-2xl sm:text-3xl md:4xl">
         {data && data.realtorForsaleQuery.autocomplete.length > 0
           ? "This is what we have found.."
-          : "No results. Try a different search query"}
+          : data && data.realtorForsaleQuery.autocomplete.length <= 0
+          ? "No results. Try a different search query."
+          : null}
       </h2>
     </div>
   );
