5b667a578b93682bbb15fe686841576dcceefb67
Anthony Salemo <ajssalemo@gmail.com>
2021-05-26 12:16:27 -0400

    fixed the height size on the landing page

--- client/realtor-airbnb/src/pages/home/home.js
+++ client/realtor-airbnb/src/pages/home/home.js
@@ -7,9 +7,9 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 
 export default function App() {
   return (
-    <div className="relative bg-black">
-      <Navbar />
-      <div className="pb-12">
+    <div className="min-h-screen relative bg-primary">
+      <div className="fixed w-full z-10">
+        <Navbar />
         <LazyLoadImages
           src={LandingBackground}
           classNames="object-center object-cover w-full h-screen img-filter"
