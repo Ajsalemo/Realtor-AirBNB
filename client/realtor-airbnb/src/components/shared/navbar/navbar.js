3730e89de9c731cc25cfa402906a9ff2fef658bc
Anthony Salemo <ajssalemo@gmail.com>
2021-06-13 19:15:36 -0400

    updated the authenticated dropdown to include users auth0 avatar

--- client/realtor-airbnb/src/components/shared/navbar/navbar.js
+++ client/realtor-airbnb/src/components/shared/navbar/navbar.js
@@ -1,11 +1,13 @@
-import AuthButton from "@components/shared/authbutton/authButton";
+import { useAuth0 } from "@auth0/auth0-react";
 import FontAwesomeLib from "@components/shared/fontawesomelib/fontAwesomeLib";
+import LazyLoadImages from "@components/shared/lazyloadimages/lazyLoadImages";
 import NavDropdown from "@components/shared/navdropdown/navDropdown";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const { user, isLoading, isAuthenticated } = useAuth0();
   const [isOpen, expandDropdown] = useState(false);
   return (
     <nav className="h-12 bg-primary border-b-2 border-white font-raleway text-white">
@@ -16,18 +18,30 @@ export default function Navbar() {
             Home
           </Link>
         </div>
-        <AuthButton
-          logInOutClassname="pr-3"
-          loadingIconClassname="animate-spin text-white mr-2"
-        />
+        <div className="flex justify-end items-center h-full pr-2">
+          <button
+            onClick={() => expandDropdown(!isOpen)}
+            onBlur={() => expandDropdown(!isOpen)}
+            className="rounded-full"
+          >
+            {isLoading ? (
+              <FontAwesomeLib
+                icon={faCircleNotch}
+                size="2x"
+                classNames="text-white animate-spin"
+              />
+            ) : user && isAuthenticated ? (
+              <LazyLoadImages
+                src={user.picture}
+                classNames="h-10 w-10 rounded-full"
+              />
+            ) : (
+              <FontAwesomeLib icon={faBars} size="2x" classNames="text-white" />
+            )}
+          </button>
+        </div>
+        <NavDropdown isOpen={isOpen} />
       </div>
-      {/* This div contains navigation links for mobile sized screens - this is hidden on tablets and larger */}
-      <div className="flex justify-end items-center h-full md:hidden pr-2">
-        <button onClick={() => expandDropdown(!isOpen)}>
-          <FontAwesomeLib icon={faBars} size="2x" classNames="text-white" />
-        </button>
-      </div>
-      <NavDropdown isOpen={isOpen} />
     </nav>
   );
 }
